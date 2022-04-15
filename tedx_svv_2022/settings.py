9d0bb84a279f9ec84853615d9d141972895d471f
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2022-04-16 00:32:09 +0530

    edit

--- tedx_svv_2022/settings.py
+++ tedx_svv_2022/settings.py
@@ -153,7 +153,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',    
 )
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = None
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Activate Django-Heroku.
